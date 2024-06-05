a8728bdf7ce9a18ea0f8e6068c0c6a2eedc20b9f
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-06-05 13:52:15 -0400

    init editor in React

--- src/components/TinyEditor.jsx
+++ src/components/TinyEditor.jsx
@@ -2,10 +2,11 @@ import './TinyEditor.css';
 import { Editor } from '@tinymce/tinymce-react';
 
 export default function TinyEditor() {
-
+  const editorRef = useRef(null);
   return (
     <Editor
       apiKey='TINY_MCE_API_KEY'
+      onInit={(_evt, editor) => editorRef.current = editor}
       init={{
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
