abe437203dd25a2895ddc75ffd5c49c5d059bb6d
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-07-10 00:14:55 -0400

    step2-update

--- src/components/TinyEditor.jsx
+++ src/components/TinyEditor.jsx
@@ -6,11 +6,11 @@ export default function TinyEditor() {
   const editorRef = useRef(null);
   return (
     <Editor
-      apiKey='TINYMCE_API_KEY'
+      apiKey='jv324dncax6j46m2cpy54igo82jhhziwdpmurp3x84e5kyuq'
       onInit={(_evt, editor) => editorRef.current = editor}
       init={{
-        plugins: 'advcode code editimage fullscreen preview wordcount math',
-        toolbar: 'undo redo blocks |  bold italic underline | strikethrough forecolor backcolor align subscript superscript | charmap blockquote | fullscreen preview math',
+        plugins: 'advcode code image editimage fullscreen preview wordcount math',
+        toolbar: 'undo redo blocks |  bold italic underline | strikethrough forecolor backcolor align subscript superscript | charmap blockquote | code fullscreen preview | image math',
       }}
       initialValue="Welcome to TinyMCE!"
     />
