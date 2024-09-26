46a36695e55fd0d254c049f854ea6b62181fcad8
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-09-26 10:30:35 -0400

    added env

--- src/components/TinyEditor.jsx
+++ src/components/TinyEditor.jsx
@@ -6,7 +6,7 @@ export default function TinyEditor() {
   const editorRef = useRef(null);
   return (
     <Editor
-      apiKey='TINY_MCE_API_KEY'
+      apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
       onInit={(_evt, editor) => editorRef.current = editor}
       init={{
         plugins: 'link image code markdown math a11ychecker tinymcespellchecker wordcount autocorrect fullscreen powerpaste preview',
