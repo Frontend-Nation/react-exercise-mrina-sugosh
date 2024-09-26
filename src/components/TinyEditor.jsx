42956ccf8aa5abd1f15714c3768c864b9f6d77ba
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-09-26 10:28:35 -0400

    update step 1

--- src/components/TinyEditor.jsx
+++ src/components/TinyEditor.jsx
@@ -6,7 +6,7 @@ export default function TinyEditor() {
   const editorRef = useRef(null);
   return (
     <Editor
-      apiKey='TINY_MCE_API_KEY'
+      apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
       onInit={(_evt, editor) => editorRef.current = editor}
       init={{
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
