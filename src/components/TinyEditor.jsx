9f67c1e50a928bdba1a5a3496b162346fef4f3f7
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-09-26 10:31:52 -0400

    updated to env

--- src/components/TinyEditor.jsx
+++ src/components/TinyEditor.jsx
@@ -6,7 +6,7 @@ export default function TinyEditor() {
   const editorRef = useRef(null);
   return (
     <Editor
-      apiKey='jv324dncax6j46m2cpy54igo82jhhziwdpmurp3x84e5kyuq'
+      apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
       onInit={(_evt, editor) => editorRef.current = editor}
       init={{
         plugins: 'advcode code image editimage fullscreen preview wordcount math',
